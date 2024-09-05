24bb52512ababd50c25da3224c8a9cd9327e9ba3
Frm2003 <felipperamos75@gmail.com>
2024-09-04 23:52:21 -0300

    add página de login

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,35 @@
+import FormComponent from '@/components/formComponent';
+import Link from 'next/link';
+
 export default function Home() {
+    const children = (
+        <>
+            <h2>Logar</h2>
+            <div>
+                <div>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="digite o email"
+                    />
+                </div>
+                <div>
+                    <label>Senha</label>
+                    <input
+                        type="password"
+                        name="senha"
+                        placeholder="digite a senha"
+                    />
+                </div>
+                <Link href={'/registro'}>Criar conta</Link>
+            </div>
+        </>
+    );
+
     return (
-        <section>
-            <h1>teste</h1>
-        </section>
+        <FormComponent url={'http://localhost:8080/user/login'}>
+            {children}
+        </FormComponent>
     );
 }
